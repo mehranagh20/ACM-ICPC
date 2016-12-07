c0dcce5cb4308d3b9b1eb8a0f47a5c082858eb7c
mehran <mehranagh200@gmail.com>
2016-12-07 13:01:41 +0330

    1230_-_MODEX added.

---
+++ 1230_-_MODEX/1230_-_MODEX.cpp
@@ -0,0 +1,58 @@
+//In The Name Of God
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef vector<vvii> vvvii;
+typedef vector<vector<viii>> vvviii;
+typedef vector<vector<iii>> vviii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+typedef vector<dd> vdd;
+
+#define inf 1000000000
+#define eps 1e-9
+
+long long fast_pow(ll a,ll n, ll mod)
+{
+    ll result = 1;
+    ll power = n;
+    ll value = a;
+    while(power>0)
+    {
+        if(power&1)
+        {result = result*value;
+            result = result%mod;}
+        value = value*value;
+        value = value%mod;
+        power /= 2;
+        //power >>= 1;
+    }
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    int tc; cin >> tc;
+    while(tc--) {
+        ll a, b, c; cin >> a >> b >> c;
+        cout << fast_pow(a, b, c) << endl;
+    }
+
+    return 0;
+}
