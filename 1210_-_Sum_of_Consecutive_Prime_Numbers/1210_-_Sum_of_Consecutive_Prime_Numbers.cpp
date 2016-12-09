a09cbf328d078053e9047155e9452377698cc25c
mehran <mehranagh200@gmail.com>
2016-12-09 21:58:52 +0330

    1210_-_Sum_of_Consecutive_Prime_Numbers added.

---
+++ 1210_-_Sum_of_Consecutive_Prime_Numbers/1210_-_Sum_of_Consecutive_Prime_Numbers.cpp
@@ -0,0 +1,67 @@
+//In The Name Of God
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef vector<vvii> vvvii;
+typedef vector<vector<viii>> vvviii;
+typedef vector<vector<iii>> vviii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+typedef vector<dd> vdd;
+
+#define inf 1000000000
+#define eps 1e-9
+
+ll sieve_size;
+bitset<10000010> bs; // 10^7 should be enough for most cases
+vi primes;
+
+// create list of primes in [0..upperbound]
+void sieve(ll upperbound) {
+    sieve_size = upperbound + 1; // add 1 to include upperbound
+    bs.set(); // set all bits to 1
+    bs[0] = bs[1] = 0; // except index 0 and 1
+    for (ll i = 2; i <= sieve_size; i++)
+        if (bs[i]) {
+            // cross out multiples of i starting from i * i!
+            for (ll j = i * i; j <= sieve_size; j += i) bs[j] = 0;
+            primes.push_back(i);
+        }
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    sieve(10001);
+    vi ans(10001, 0);
+    for(int i = 0; i < primes.size(); i++) {
+        int tillNow = 0;
+        for (int j = i; j < primes.size(); j++) {
+            tillNow += primes[j];
+            if(tillNow > 10000) break;
+            ans[tillNow]++;
+        }
+    }
+    int n;
+    while(cin >> n && n) {
+        cout << ans[n] << endl;
+    }
+
+    return 0;
+}
