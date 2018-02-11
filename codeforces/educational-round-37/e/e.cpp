fab3acb7c27518b52091b1829475081f8e650c21
Mehran <mehranagh200@gmail.com>
2018-02-11 11:45:54 +0330

    Complete educational round 37

--- codeforces/educational-round-37/e/e.cpp
+++ codeforces/educational-round-37/e/e.cpp
@@ -28,8 +28,45 @@ typedef vector<dd> vdd;
 #define inf 1000000000
 #define eps 1e-9
 
+set<ii> prevv;
+set<int> nvis;
+
+int dfs(int a) {
+    int cur = 1;
+    nvis.erase(a);
+    for(auto it = nvis.begin(); it != nvis.end();) {
+        int x = *it;
+        if(prevv.find(ii(a, x)) == prevv.end()) {
+            cur += dfs(*it);
+            it = nvis.upper_bound(x);
+        }
+        else it++;
+    }
+    return cur;
+}
+
+
 int main() {
     ios::sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int a, b; cin >> a >> b;
+        a--, b--;
+        prevv.insert(ii(a, b)), prevv.insert(ii(b, a));
+    }
+    for(int i = 0; i < n; i++) {
+        nvis.insert(i);
+    }
+
+    vi ans;
+    for(int i = 0; i < n; i++) {
+        if(nvis.count(i)) ans.push_back(dfs(i));
+    }
+    sort(ans.begin(), ans.end());
+
+    cout << ans.size() << endl << ans[0];
+    for(int i = 1; i < ans.size(); i++) cout << " " << ans[i];
+
 
 
     return 0;
--- codeforces/educational-round-37/f/f.cpp
+++ codeforces/educational-round-37/f/f.cpp
@@ -28,8 +28,63 @@ typedef vector<dd> vdd;
 #define inf 1000000000
 #define eps 1e-9
 
+vector<ll> seg;
+
+ll sum(int p, int l, int r, int wl, int wr) {
+    if(wr < l || wl > r) return 0;
+    if(l >= wl && r <= wr) return seg[p];
+    return sum(p * 2, l, (r + l) / 2, wl, wr) + sum(p * 2 + 1, (r + l) / 2 + 1, r, wl, wr);
+}
+
+void update(int x, int i, int p, int l, int r) {
+    if(i == l && i == r) {
+        seg[p] = x;
+        return;
+    }
+    if(i < l || i > r) return;
+    update(x, i, p * 2, l, (l + r) / 2);
+    update(x, i, p * 2 + 1, (l + r) / 2 + 1, r);
+    seg[p] = seg[p * 2] + seg[p * 2 + 1];
+}
+
 int main() {
-    ios::sync_with_stdio(0);
+    int n, m, mx = 1e6 + 100;
+    scanf("%d%d", &n, &m);
+    vi numdiv(mx, 1);
+    set<ii> s;
+    seg.assign(mx * 3, 0);
+    for (int k = 0; k < n; ++k) {
+        int a; cin >> a;
+        update(a, k, 1, 0, n - 1);
+        if(a >= 3)
+            s.insert(ii(k, a));
+    }
+
+    for (int i = 2; i < mx; i++)
+            for (int j = i; j < mx; j += i)
+                numdiv[j]++;
+
+    while(m--) {
+        int t, r, l; scanf("%d%d%d", &t, &l, &r);
+        l--, r--;
+        if(t == 1) {
+            while(1) {
+                auto it = s.lower_bound(ii(l, -1));
+                if(it == s.end() || it->first > r) break;
+                update(numdiv[it->second], it->first, 1, 0, n - 1);
+                l = it->first + 1;
+                if(numdiv[it->second] < 3) s.erase(it);
+                else {
+                    ii t = *it;
+                    s.erase(it);
+                    s.insert(ii(t.first, numdiv[t.second]));
+                }
+            }
+        }
+        else {
+            printf("%lld\n", sum(1, 0, n - 1, l, r));
+        }
+    }
 
 
     return 0;
